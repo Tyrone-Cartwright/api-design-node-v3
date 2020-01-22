b1d5c3f19003aab10dd7e4b98eba937e63387998
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2020-01-22 09:37:32 -0500

    overview of routes, REST, Middleware, Route Order and setting up Express Routes

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+// creates a router
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -12,4 +14,27 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+router.get('/me', (req, res) => {
+  res.send({ me: 'hello' })
+})
+
+app.use('/api', router)
+// CRUD
+app.get('/', (req, res, next) => {
+  // res.send({ data: 1 })
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.send({ data: 2 })
+})
+
+app.post('/data', (req, res) => {
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('app running on port 3000')
+  })
+}
